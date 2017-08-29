d4c8e22634d11d6914f88664b60ae72286f36ad4
CedricSchoeni <cedricschoeni123@gmx.ch>
2017-08-29 11:23:30 +0200

    first commit

---
+++ GUITravel_Modul120/index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<TITLE> 6 </TITLE>
+<link href="main.css" type="text/css" rel="stylesheet">
+
+<script type="text/javascript">
+	alert("ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\nYOU'RE PC HAS BEEN INFECTED WITH DANGARAUS WIRUS.\nPLEASE CALL 012393939393 FOR PROFFESIONAL\n(indian) TECHSUPPORT WITH MICROSOFT CERTIFICATE");
+</script>
+</HEAD>
+<BODY>
+	<p class="green">
+		name fejj
+	</p>
+</BODY>
+</HTML>
+
+
+
+
+
+
+
+
+
+
